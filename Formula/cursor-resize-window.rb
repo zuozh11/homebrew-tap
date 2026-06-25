d7fc0e8103713b7cb89c0b3e9648a9d15833e2bd
左智 <zuozhi@szlanyou.com>
2026-06-25 17:33:10 +0800

    Add cursor-resize-window formula

---
+++ Formula/cursor-resize-window.rb
@@ -0,0 +1,25 @@
+class CursorResizeWindow < Formula
+  desc "Resize macOS windows with ctrl-left-drag"
+  homepage "https://github.com/zuozh11/cursor-resize-window"
+  url "https://github.com/zuozh11/cursor-resize-window/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "9adcf8f5501ecaf65020fe23e45b993084479c70deabeb11d4d677812bfe65e3"
+  license "MIT"
+
+  depends_on xcode: ["15.0", :build]
+
+  def install
+    system "swift", "build", "--configuration", "release", "--disable-sandbox"
+    bin.install ".build/release/cursor-resize-window"
+  end
+
+  service do
+    run [opt_bin/"cursor-resize-window"]
+    keep_alive true
+    log_path var/"log/cursor-resize-window.log"
+    error_log_path var/"log/cursor-resize-window.log"
+  end
+
+  test do
+    assert_predicate bin/"cursor-resize-window", :executable?
+  end
+end
